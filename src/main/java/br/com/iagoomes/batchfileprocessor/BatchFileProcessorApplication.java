2fad46a706bef2c10213148aa18fbfa993d8aa23
Iago Gomes <iagoomes@outlook.com>
2025-05-24 13:32:24 -0300

    feat: project initialization with Spring Boot base setup and main dependencies

---
+++ src/main/java/br/com/iagoomes/batchfileprocessor/BatchFileProcessorApplication.java
@@ -0,0 +1,13 @@
+package br.com.iagoomes.batchfileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchFileProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchFileProcessorApplication.class, args);
+    }
+
+}
